f49d696d75475a316c0c2352010be20caa850b1e
Snigdho Dip Howlader <snigdho.howlader@gmail.com>
2023-11-13 16:29:36 +0600

    Adding update for serialization check

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import authSlice from "./auth";
-import { persistReducer, persistStore } from "redux-persist";
+import { PERSIST, persistReducer, persistStore } from "redux-persist";
 import localStorage from "redux-persist/lib/storage";
 
 const reducers = combineReducers({
@@ -17,6 +17,12 @@ const persistedReducer = persistReducer(persistConfig, reducers);
 
 const store = configureStore({
     reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+        serializableCheck: {
+            ignoredActions: [PERSIST],
+        },
+    }),
 });
 
 const persistor = persistStore(store);
